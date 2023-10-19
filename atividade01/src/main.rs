44c613e1a9eaf6ed58a8085f4b40832cb037386d
leleosilva <leo.hhcsilva@gmail.com>
2023-10-19 09:52:25 -0300

    Adicionada a capacidade de escolher e criar um diretório para salvar imagens

--- atividade01/src/main.rs
+++ atividade01/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::create_dir_all;
 use image;
 
 // Gerenciador que salva imagens no diretório de armazenamento definido por 'save_path'
@@ -7,17 +8,20 @@ pub struct ImgManager {
 
 impl ImgManager {
 
-    // Cria uma nova instância do ImgManager
+    // Cria e retorna uma nova instância do ImgManager e um novo diretório caso ele ainda não exista
     pub fn new(path: String) -> Self {
+        create_dir_all(&path).expect("Não foi possível criar o diretório desejado.");
         Self { save_path: path }
     }
 
-    // Recebe uma imagem RGB e a salva em 'save_path'
-    pub fn save_png(&self, img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>)
+    // Recebe uma imagem RGB e a salva em 'save_path', no formato PNG
+    pub fn save_png(&self, img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, img_name: &str)
         -> Result<(), image::ImageError> {
-        img.save_with_format(&self.save_path, image::ImageFormat::Png)
+
+        let mut path = self.save_path.to_owned();
+        path.push_str(img_name);
+        img.save_with_format(path, image::ImageFormat::Png)
     }
-    
 }
 
 fn main() {
@@ -32,7 +36,7 @@ mod tests {
     /* Cria uma imagem no estilo gradiente de nome 'gradient_img.png',
      * verificando se foi possível salvá-la corretamente */
     fn test_create_gradient_img() {
-        let img_manager = ImgManager::new(String::from("gradient_img.png"));
+        let img_manager = ImgManager::new(String::from("./imgs/"));
 
         const IMAGE_WIDTH: u32 = 256;
         const IMAGE_HEIGHT: u32 = 256;
@@ -53,6 +57,6 @@ mod tests {
                 img.put_pixel(i, j, image::Rgb([ir, ig, ib]));
             }
         }
-        assert_eq!(img_manager.save_png(img).is_ok(), true);
+        assert_eq!(img_manager.save_png(img, "gradient_img.png").is_ok(), true);
     }
 }
